6b5375b9ed2989f5e7f55e2949a91973667c5975
Fábio Varela <fabio-varela@protonmail.com>
2023-11-07 21:20:52 -0300

    UI changes to appfeed

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import AppFeed from '@components/AppFeed'
 function Home() {
 
   return ( 
-      <section className='max-w-4xl p-6 mt-6'>
+      <section style={{maxWidth:'80rem', marginTop:'10px'}}>
         
         <AppFeed/> 
 

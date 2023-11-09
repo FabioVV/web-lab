436e3db3b98c6ce29f03585c21892e94519bd503
Fábio Varela <fabio-varela@protonmail.com>
2023-11-09 20:47:46 -0300

    WIP #4 pagination

--- components/Pagination_component.js
+++ components/Pagination_component.js
@@ -3,12 +3,17 @@ import React from 'react'
 function Pagination({page_size, count, total_pages, current_page_number, next, previous, fetch, url}) {
     
     const regex = /(page=\d+)/g;
-    let links = []
-
-
     let next_r = next?.match(regex)?.toString()
+    let previous_r = previous?.match(regex)?.toString()
+
+    let links = []
+    let links_new = []
     let new_next = ''
     let page_value_next = ''
+    let new_previous = ''
+    let page_value_previous = ''
+
+
     if(next_r){
         page_value_next = parseInt(next_r[next_r?.length-1]) + 1
         if(page_value_next <= total_pages){
@@ -16,10 +21,6 @@ function Pagination({page_size, count, total_pages, current_page_number, next, p
         } 
     }
 
-
-    let previous_r = previous?.match(regex)?.toString()
-    let new_previous = ''
-    let page_value_previous = ''
     if(previous_r){
         page_value_previous = parseInt(previous_r[previous_r?.length-1]) - 1
         if(page_value_previous >= 1){
@@ -47,34 +48,28 @@ function Pagination({page_size, count, total_pages, current_page_number, next, p
         </button>)
     }
 
+    //{page_size, count, total_pages, current_page_number, next, previous, fetch, url}
+
     if(links.length > 4){
+        links_new = links.slice(current_page_number, total_pages)
 
-        links = []
-
-        for (let index = 0; index < total_pages; index++) {
-            links.push(<button onClick={async ()=>{await fetch(`${url}?page=${index+1}`); window.scrollTo({ top: 0, behavior: 'smooth' }); }} 
-            className={current_page_number === index+1  ? "join-item btn btn-active":"join-item btn"}>
-                {index+1}
-            </button>
-            )            
-        }
-
-
-    } 
+    }   
 
   return ( 
     <div className='mt-3 mb-8 text-center'>
         <div className="join">
 
             <button onClick={async ()=>{await fetch(`${previous}`); window.scrollTo({ top: 0, behavior: 'smooth' });} } className="join-item btn bg-black">&lt;</button>
-            <button onClick={()=>{new_previous_fetch(new_previous); window.scrollTo({ top: 0, behavior: 'smooth' }); }} className={current_page_number === page_value_previous  ? "join-item btn btn-active":"join-item btn"}> &lt;&lt;</button>
-            {links} 
-            <button onClick={()=>{new_next_fetch(new_next); window.scrollTo({ top: 0, behavior: 'smooth' }); }} className={current_page_number === page_value_next  ? "join-item btn btn-active":"join-item btn"}>&gt;&gt;</button>
+            <button onClick={async ()=>{await new_previous_fetch(new_previous); window.scrollTo({ top: 0, behavior: 'smooth' }); }} className={current_page_number === page_value_previous  ? "join-item btn btn-active":"join-item btn"}> &lt;&lt;</button>
+            {links_new ? links : links} 
+            <button onClick={async ()=>{await new_next_fetch(new_next); window.scrollTo({ top: 0, behavior: 'smooth' }); }} className={current_page_number === page_value_next  ? "join-item btn btn-active":"join-item btn"}>&gt;&gt;</button>
             <button onClick={async ()=>{await fetch(`${next}`); window.scrollTo({ top: 0, behavior: 'smooth' });}} className="join-item btn bg-black">&gt;</button>
        
         </div>
         <div>
             <span className='font-bold'>Total de páginas: {total_pages ? total_pages: '1'}</span>
+            <br/>
+            <span className='font-bold'>Mostrando página {current_page_number} de {total_pages}</span>
         </div>
   </div>
   )

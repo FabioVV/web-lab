84bfa4d35e7cb79730e24e87d6ab1c567e0e199c
FabioVV <fabio-varela@protonmail.com>
2023-11-14 11:45:48 -0300

    fixed nav color dropdown color, lab updated text

--- components/Laboratory.jsx
+++ components/Laboratory.jsx
@@ -52,7 +52,20 @@ function Laboratory({lab, HandleFetch}) {
                 </div> */}
                 <div>
                     <div className="font-bold">{lab.name}</div>
-                    <div className="text-sm opacity-50">--------</div>
+                    <div style={{cursor:'help'}} className="text-sm opacity-50">
+                        {lab.updated_at && lab.updated_at != lab.created_at 
+                            ? 
+                                <>
+                                    Laboratório foi atualizada em:
+                                    <div title={`'Os dados do laboratório foram alterados.`} style={{display:'flex', flexDirection:'row', gap:'5px'}}>
+                                        <svg style={{cursor:'pointer'}}  xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                                        {lab.updated_at.replaceAll('-', '/')}
+                                    </div>
+                                </>
+                            : 
+                            'Laboratório não foi atualizado.'
+                        }
+                    </div>
                 </div>
             </div>
             </td>
